7eb7a549b49c6e3851b1c41d1a5533b6f71aeeb2
Goupil <goupiljeremy@gmail.com>
2020-01-22 14:01:33 +0100

    implement the function makeDeposit to pass the test

--- src/main/java/org/example/BankAccount.java
+++ src/main/java/org/example/BankAccount.java
@@ -3,7 +3,7 @@ package org.example;
 public class BankAccount {
     private int accountStatement = 0;
     public void makeDeposit(int deposit) {
-
+        accountStatement += deposit;
     }
 
     public int getAccountStatement() {
