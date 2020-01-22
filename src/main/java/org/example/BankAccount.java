bdee6deed5222205f5cb4e895f960c191ae38aeb
Goupil <goupiljeremy@gmail.com>
2020-01-22 13:35:08 +0100

    initial commit and US 1

---
+++ src/main/java/org/example/BankAccount.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class BankAccount {
+    private int accountStatement = 0;
+    public void makeDeposit(int deposit) {
+
+    }
+
+    public int getAccountStatement() {
+        return accountStatement;
+    }
+}
---
+++ src/test/java/org/example/BankAccountTest.java
@@ -0,0 +1,63 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ *  Think of your personal bank account experience When in doubt, go for the simplest solution
+ *
+ *
+ *
+ * Requirements
+ *
+ *
+ *
+ * Deposit and Withdrawal
+ *
+ * Account statement (date, amount, balance)
+ *
+ * Statement printing
+ *
+ *
+ *
+ * User Stories
+ *
+ *
+ *
+ * US 1:
+ * In order to save money
+ * As a bank client
+ * I want to make a deposit in my account
+ *
+ *
+ *
+ * US 2:
+ * In order to retrieve some or all of my savings
+ * As a bank client
+ * I want to make a withdrawal from my account
+ *
+ *
+ *
+ * US 3:
+ * In order to check my operations
+ * As a bank client
+ * I want to see the history (operation, date, amount, balance)  of my operations
+ */
+
+class BankAccountTest {
+
+    /* US 1:
+    * In order to save money
+    * As a bank client
+    * I want to make a deposit in my account
+    */
+    @Test
+    void shouldMakeADepositInMyAccount(int deposit){
+        // given
+        BankAccount bankAccount = new BankAccount();
+        // when
+        bankAccount.makeDeposit(deposit);
+        // then
+        assertEquals(deposit, bankAccount.getAccountStatement());
+    }
+}
